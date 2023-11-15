7355e1f8b2f2742e6aac835d95e61c41894cd54a
Rao Nishchith Niranjan <nishchitrao5@gmail.com>
2023-11-15 09:30:05 +0530

    chore: add a sample app for showcase and development

---
+++ example/metro.config.js
@@ -0,0 +1,25 @@
+const {withNxMetro} = require('@nx/react-native');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const path = require('path');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const {assetExts, sourceExts} = defaultConfig.resolver;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const customConfig = {};
+
+module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
+  // Change this to true to see debugging info.
+  // Useful if you have issues resolving modules
+  debug: false,
+  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx', 'json'
+  extensions: [],
+  // Specify folders to watch, in addition to Nx defaults (workspace libraries and node_modules)
+  watchFolders: [],
+});
